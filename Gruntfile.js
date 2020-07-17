2b3dcb735875ef9c93449fe7eab36e6279c8a87e
@jlopezlira <jlopezlira@gmail.com>
2020-07-16 19:15:53 -0500

    ESlint + Prettier setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,6 +1,6 @@
 module.exports = function(grunt) {
   grunt.initConfig({
-    pkg: grunt.file.readJSON("package.json"),
+    pkg: grunt.file.readJSON('package.json'),
     sass: {
       // dist: {
       //     options: {
@@ -19,14 +19,14 @@ module.exports = function(grunt) {
       // },
       dist2: {
         options: {
-          style: "expanded",
-          require: "sass-globbing",
+          style: 'expanded',
+          require: 'sass-globbing'
         },
         files: {
-          "css/app.css": "scss/app.scss",
-          "css/icons.css": "scss/icons.scss",
-        },
-      },
+          'css/app.css': 'scss/app.scss',
+          'css/icons.css': 'scss/icons.scss'
+        }
+      }
     },
     autoprefixer: {
       // dist: {
@@ -42,37 +42,37 @@ module.exports = function(grunt) {
       // },
       dist2: {
         options: {
-          map: true,
+          map: true
         },
         files: {
-          "css/app.css": "css/app.css",
-          "css/icons.css": "css/icons.css",
-        },
-      },
+          'css/app.css': 'css/app.css',
+          'css/icons.css': 'css/icons.css'
+        }
+      }
     },
     watch: {
       css: {
         // files: 'scss/**/*.scss',
-        files: "scss/*.scss",
-        tasks: ["sass", "autoprefixer"],
+        files: 'scss/*.scss',
+        tasks: ['sass', 'autoprefixer']
       },
       options: {
-        livereload: true,
-      },
+        livereload: true
+      }
     },
     connect: {
       server: {
         options: {
           port: 8080,
-          hostname: "*",
-        },
-      },
-    },
-  });
-  grunt.loadNpmTasks("grunt-sass-globbing");
-  grunt.loadNpmTasks("grunt-contrib-sass");
-  grunt.loadNpmTasks("grunt-contrib-watch");
-  grunt.loadNpmTasks("grunt-autoprefixer");
-  grunt.loadNpmTasks("grunt-contrib-connect");
-  grunt.registerTask("default", ["connect", "watch"]);
-};
+          hostname: '*'
+        }
+      }
+    }
+  })
+  grunt.loadNpmTasks('grunt-sass-globbing')
+  grunt.loadNpmTasks('grunt-contrib-sass')
+  grunt.loadNpmTasks('grunt-contrib-watch')
+  grunt.loadNpmTasks('grunt-autoprefixer')
+  grunt.loadNpmTasks('grunt-contrib-connect')
+  grunt.registerTask('default', ['connect', 'watch'])
+}
